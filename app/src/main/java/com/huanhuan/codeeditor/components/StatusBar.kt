6d1f62a8304e847858e3daacd6e4925a0d940661
_huanhuan_ <138788868+huanhuan0812@users.noreply.github.com>
2025-10-15 09:57:56 +0800

    更新 StatusBar.kt

--- app/src/main/java/com/huanhuan/codeeditor/components/StatusBar.kt
+++ app/src/main/java/com/huanhuan/codeeditor/components/StatusBar.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 //import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
