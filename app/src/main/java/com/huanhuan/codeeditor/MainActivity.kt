fc937f8e0c06c6d8d708c24c5ee6b592879a7ae0
_huanhuan_ <138788868+huanhuan0812@users.noreply.github.com>
2025-10-15 09:23:44 +0800

    更新 MainActivity.kt

--- app/src/main/java/com/huanhuan/codeeditor/MainActivity.kt
+++ app/src/main/java/com/huanhuan/codeeditor/MainActivity.kt
@@ -38,14 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun setImmersiveMode() {
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN
-                )
+    WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 
     override fun onResume() {
