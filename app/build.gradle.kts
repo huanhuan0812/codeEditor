f0fcaf5a2fc8804232a6708dc12a4a89b620901a
_huanhuan_ <138788868+huanhuan0812@users.noreply.github.com>
2025-10-15 09:37:57 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation("androidx.compose.material:material-icons-core")
     implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
